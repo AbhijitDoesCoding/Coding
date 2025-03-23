9720ae1c6a5dd3a79515642c06d6f28986a2b4c9
Abhijit <abalpande1804@gmail.com>
2025-03-23 15:31:48 +0530

    save

---
+++ JS/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 1234;
+let accountEmail = "abalpande1804@gmail.com"
+var accountPassword = "password1234"
+accountCity = "Pune"
+// accountID = 5678
+accountEmail = "hc"
+accountCity = "Mumbai"
+
+let accountstate
+console.log(accountID, accountEmail, accountPassword, accountCity, accountstate);
+// console.table({ accountID, accountEmail, accountPassword, accountCity });
+
+
+
+
+
+
